c118be6fe58cb63a6f16581ff8d0cff5ea68f9ee
Abir-Zayn <abirzayn561@gmail.com>
2025-10-30 10:42:25 +0600

    task: locally host the schema in mongodb : #2

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+const app = express();
+
+import cookieParser from 'cookie-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import './config/mongoose-connection.js';
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('index', { title: 'Home' });
+});
+
+app.listen(3000);
---
+++ models/product-schema.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/oneTDB") 
+
+// E-Commerce Product Model 
+const productSchema =  mongoose.Schema({
+    name: String,
+    description: String,
+    price: Number,
+    discount: {
+        type: Number,
+        default: 0
+    },
+    bgColor: String,
+    panelColor: String,
+    textColor: String,
+    category: String,
+    image: String,    
+    stock: Number,   //extra
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }, 
+    updatedAt: {         //extra
+        type: Date,
+        default: Date.now
+    },
+});
+
+
+module.exports = mongoose.model('product', productSchema);
